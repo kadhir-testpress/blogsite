20923d1f910cc3bde26288cf7cc9b2ac6e6908f9
kadhir <kadhir@example.com>
2024-02-12 14:29:32 +0530

    Add views and Templates

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 class PublishedManager(models.Manager):
     # adding custom manager to Post Model
@@ -48,4 +49,13 @@ class Post(models.Model):
     def __str__(self):
         # Return a human-readable string representation of the post
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug
+                       ])
+    
 
