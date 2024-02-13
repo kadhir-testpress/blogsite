5101217f3653b61c9bd02eb4a789bdb9745fe1d6
kadhir <kadhir@example.com>
2024-02-13 12:26:13 +0530

    Comment Models and Forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,16 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
+    #Form to share post
     name = forms.CharField(max_length=40)
     email = forms.EmailField()
     to = forms.EmailField()
     comments = forms.CharField(required=False,
                                 widget=forms.Textarea)
+    
+class CommentForm(forms.ModelForm):
+    #Comment model form 
+    class Meta:
+        model = Comment
+        fields = ('name', 'email', 'body')
