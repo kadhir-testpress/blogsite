08b9d3a7ad59d9d88b3f294220842438e13c2a8a
kadhir <kadhir@kadhir.com>
2024-02-15 12:35:25 +0530

    Trigram similarity

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, get_object_or_404 
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from .models import Post
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.contrib.postgres.search import TrigramSimilarity
